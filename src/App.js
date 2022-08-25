884e4d4842a80c06f4d3fce22db1f9e1f2e2d08f
GUchechi <godswilluchendu7@gmail.com>
2022-08-25 02:52:04 +0100

    added config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+    const getJoke = () => {
+
+      const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+      Axios.get('https://icanhazdadjoke.com', config).then((response) => {
+        console.log(response.data);
+      })
+    }
+
   return (
     <div className="App">
-     Hello Youtube
+     Hello Youtube <button onClick={getJoke}>Get Joke</button>
     </div>
   );
 }
